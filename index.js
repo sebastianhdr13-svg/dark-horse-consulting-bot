0b9387bb8f61364978822cc5810f547bb1182050
sebastianhdr13-svg <sebastianhdr13@gmail.com>
2026-09-04 10:08:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -194,7 +194,7 @@ async function createPrivateChannel(member) {
             content:
               `@everyone please give a warm welcome to the new legend <@${member.id}>! We're all hyped to have you 🤙 Please introduce yourself and connect with the rest of the legends...\n\n` +
               `1. Drop your Instagram handle\n` +
-              `2. Drop your goal(s) for the next 3 months inside the program\n` +
+              `2. Drop your goal(s) for the next 4 months inside the program\n` +
               `3. Let us know more about yourself (anything you'd like)\n\n` +
               `Time to take action and RIP 💰`,
             allowedMentions: { parse: ['everyone', 'users'] },
